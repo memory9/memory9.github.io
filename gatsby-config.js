e125b3633f6a6bcc59a01f3183652c06314a868f
mingrui <474562318@qq.com>
2019-04-29 09:35:56 +0800

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `Cube Blog`,
+    author: `memory`,
+    description: `A Personal blog by mingrui.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
       twitter: `kylemathews`,
